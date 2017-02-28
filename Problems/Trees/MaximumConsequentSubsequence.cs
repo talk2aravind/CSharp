f2f5f02b98d600d30c584895921cd4f2082c4485
aravp <aravp@amazon.com>
2017-02-28 00:35:13 -0800

    Add comment to finding max sequence.

--- Problems/Trees/MaximumConsequentSubsequence.cs
+++ Problems/Trees/MaximumConsequentSubsequence.cs
@@ -5,7 +5,6 @@ namespace Trees
 {
     public class MaximumConsequentSubsequence
     {
-
         public int Find(TreeNode<int> root) => Find(root, -9999, 0);
 
         private int Find(TreeNode<int> root, int prevValue, int currentCount)
@@ -13,14 +12,16 @@ namespace Trees
             if (root == null)
                 return currentCount;
 
+            // Increment count if necessary.
             if (prevValue + 1 == root.Data)
                 currentCount++;
             else
-                currentCount = 1;
+                currentCount = 1; // Reset count back to 1 if out of sequence.
 
             var leftCount = Find(root.Left, root.Data, currentCount);
             var rightCount = Find(root.Right, root.Data, currentCount);
 
+            // Pick the best count as winner.
             return new[] { currentCount, leftCount, rightCount }.Max();
         }
     }
